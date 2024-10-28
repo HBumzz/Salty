a0df38a65cde1760190b687c59072e3633c93796
afteryoon <khsso102649@gmail.com>
2024-10-28 22:53:51 +0900

    Feat : 회원 가입 및, 로그인 기능

---
+++ src/main/java/com/app/salty/user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.app.salty.user.repository;
+
+import com.app.salty.user.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+
+    boolean existsByEmail(String email);
+
+    Optional<Users> findByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
