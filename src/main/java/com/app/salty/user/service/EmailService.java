46bd081588d4580805009c3fb2bf57eb785e79ff
afteryoon <khsso102649@gmail.com>
2024-10-28 23:19:32 +0900

    Feat : 이메일 인증 및 레디스 인증값 저장

---
+++ src/main/java/com/app/salty/user/service/EmailService.java
@@ -0,0 +1,54 @@
+package com.app.salty.user.service;
+
+import com.app.salty.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final RedisTemplate<String, String> redisTemplate;
+    private final UserRepository userRepository;
+    private final JavaMailSender emailSender;
+    private static final long VERIFICATION_CODE_EXPIRATION = 300L;
+
+    //메일 전송 및 코드 저장
+    public String sendVerificationEmail(String email) {
+        String verificationCode = generateVerificationCode();
+        redisTemplate.opsForValue()
+                .set(getRedisKey(email), verificationCode,
+                        Duration.ofSeconds(VERIFICATION_CODE_EXPIRATION));
+
+        sendEmail(email, verificationCode);
+        return verificationCode;
+    }
+
+    // 인증 코드 확인
+    public boolean verifyCode(String email, String code) {
+        String storedCode = redisTemplate.opsForValue().get(getRedisKey(email));
+        return code.equals(storedCode);
+    }
+
+
+    private String getRedisKey(String email) {
+        return "EMAIL_VERIFICATION:" + email;
+    }
+
+    private String generateVerificationCode() {
+        return String.format("%06d", new Random().nextInt(999999));
+    }
+
+    private void sendEmail(String to, String code) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("이메일 인증 코드");
+        message.setText("인증 코드: " + code);
+        emailSender.send(message);
+    }
+}
---
+++ src/main/java/com/app/salty/user/service/UserService.java
@@ -0,0 +1,67 @@
+package com.app.salty.user.service;
+
+
+import com.app.salty.config.globalExeption.custom.DuplicateEmailException;
+import com.app.salty.user.common.Role;
+import com.app.salty.user.dto.request.UserSignupRequest;
+import com.app.salty.user.dto.response.UserResponse;
+import com.app.salty.user.entity.Users;
+import com.app.salty.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserResponse signup(UserSignupRequest request) {
+        validateDuplicateEmail(request.getEmail());
+
+        Users signupUser = userSignupRequestToUser(request);
+        signupUser.updateRole(Role.USER);
+        signupUser.updateActivated(true);
+
+        signupUser =  userRepository.save(signupUser);
+        log.info("signUser : {}", signupUser);
+
+        return convertUserResponse(signupUser);
+    }
+
+    //convert
+    private Users userSignupRequestToUser(UserSignupRequest request) {
+        return Users.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .nickname(request.getNickname())
+                .build();
+    }
+
+    private UserResponse convertUserResponse(Users user){
+        return UserResponse.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+    }
+
+    //이메일 중복 검사
+    private void validateDuplicateEmail(String email) {
+        if(userRepository.existsByEmail(email))
+            throw new DuplicateEmailException("이미 가입된 이메일입니다");
+    }
+
+    public boolean verifyNickname(String nickname) {
+        return !userRepository.existsByNickname(nickname);
+    }
+}
